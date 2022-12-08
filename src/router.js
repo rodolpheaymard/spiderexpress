75e00b9d86c6ea095053d005a215192d6c5eb3c1
Rodolphe Aymard <rodolpheaa@yahoo.fr>
2022-12-08 14:55:08 +0100

    all by 4

--- src/router.js
+++ src/router.js
@@ -41,6 +41,15 @@ router.get("/all/:objecttype1/:objecttype2/:objecttype3", (req,res)=>{
   res.json({ list1 : result1, list2 : result2, list3 : result3});
 })
 
+router.get("/all/:objecttype1/:objecttype2/:objecttype3/:objecttype4", (req,res)=>{
+  let result1 = mymodel.getObjects(req.params.objecttype1);
+  let result2 = mymodel.getObjects(req.params.objecttype2);
+  let result3 = mymodel.getObjects(req.params.objecttype3);
+  let result4 = mymodel.getObjects(req.params.objecttype4);
+
+  res.json({ list1 : result1, list2 : result2, list3 : result3, list4 : result4});
+})
+
 router.post('/add/:objecttype', (req, res) => {
   let newobj = req.body;
   mymodel.addObject(newobj, req.params.objecttype);
